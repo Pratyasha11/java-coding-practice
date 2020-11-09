4eebe192cc23e0e463ccd2d6e33e416c3f63c9e7
Pratyasha <pratyashanp192@gmail.com>
2020-11-09 12:39:22 +0530

    understanding For Loop

---
+++ ForLoop.java
@@ -0,0 +1,31 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+/*
+ * In while an d do while we are write codes in multiple line
+ * but using for loop we can write it in a single line 
+ */
+		
+		for(int i=2;i<=5;i++)
+		{
+			System.out.println("Hello");
+		}
+		/*
+		 * when you know the starting and ending point, always use for loop
+		 * like i have to start from 1 and end at 5 in the above
+		 * 
+		 * but when you don't know the starting and ending point
+		 * we have to iterate a loop depends upon some other factors
+		 * suppose you are developing a chating application
+		 * and you're taking from the user continuously
+		 * till then when user doesn't input a value like exit
+		 * therefore in while condition, you have to specify a condition 
+		 * that when the user use exit,it will stop
+		 * 
+		 * for-each is used for arrays
+		 */
+
+	}
+
+}
