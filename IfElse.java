0196344cbbeb8803636ac665a9ac2d0939a3bb5c
Pratyasha <pratyashanp192@gmail.com>
2020-11-09 12:24:13 +0530

    Basic program about IfElse

---
+++ IfElse.java
@@ -0,0 +1,27 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		int n=7;
+		
+//		if(true)
+//		System.out.println("hello");
+//		
+//		
+//		if(false)
+//		System.out.println("bye");
+		
+		
+//		if(n%2==0)
+//			System.out.println("Even");
+//		
+//		if(n%2!=0)
+//			System.out.println("Odd");
+
+		if(n%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+
+}
