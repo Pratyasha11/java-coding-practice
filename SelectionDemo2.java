e4b5fd2afc6ce1220cdc970c9ce3099c213fe9c4
Pratyasha <pratyashanp192@gmail.com>
2020-11-09 12:32:00 +0530

    another basic program regarding switchcase | break

---
+++ SelectionDemo2.java
@@ -0,0 +1,18 @@
+
+public class SelectionDemo2 {
+
+	public static void main(String[] args) {
+		String n ="xyz";
+		switch(n)
+		{
+		case  "abc":
+			System.out.println("yes");	
+			break;
+		case "pqr":
+			System.out.println("no");
+			break;
+			default:
+				System.out.println("nothing");
+	}
+}
+}
