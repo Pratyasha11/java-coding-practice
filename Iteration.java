873c52b51633ae1a48baffccb8ad52867fd66d38
Pratyasha <pratyashanp192@gmail.com>
2020-11-09 12:35:40 +0530

    added a simple program regarding iteration

---
+++ Iteration.java
@@ -0,0 +1,28 @@
+
+public class Iteration {
+
+	public static void main(String[] args) {
+		// Iteration: when you want to repeat something repeatedly
+//		when you repeat any code, it is code redundancy. It's a crime
+//		we have to use a loop,i.e: while,do while,for,for-each
+		
+		/*
+		 * while(true)
+		 * {
+		 * System.out.println("Hello");
+		 * }
+		 */		
+		
+//		It will print hello infinite times
+//		suppose i want to print only 5 times, then you need a counter
+//		counter will count it for you
+		
+		int i=3;
+		while(i<=5) { //counter applied
+			System.out.println("Hello");
+			i++;
+		}
+	}
+	}
+
+
