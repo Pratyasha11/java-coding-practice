c037a6f8ac68e6540d02bcc7385ee40feaf5a7be
Pratyasha <pratyashanp192@gmail.com>
2020-11-09 12:41:54 +0530

    number pattern basic using nested loop

---
+++ NestedLoop2.java
@@ -0,0 +1,13 @@
+
+public class NestedLoop2 {
+
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 4; i++) {
+			for (int j = 1; j <= 4; j++) {
+				System.out.print(j  + " "); //if I want to print no
+			}
+			System.out.println();
+		}
+	}
+}
